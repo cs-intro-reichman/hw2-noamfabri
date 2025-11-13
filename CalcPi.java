a92ba6c2ecfc7658607417114f36e0fc9cb45082
noamfabri87 <noamfabri87@gmail.com>
2025-11-13 09:26:07 +0200

    Pi, done

--- CalcPi.java
+++ CalcPi.java
@@ -2,14 +2,17 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 		int N = Integer.parseInt(args[0]);
-		int i = 0;
-		int P = 	
-		double miPI = 1 \ P;
+		int i = 0;	
+		double denominator = 1;
+		double sign = 1.0;
+		double totalPi = 0;
 		System.out.println("pi according to java:" + Math.PI);
 		while (i < N) {
-
-			
-			
+			totalPi = totalPi + ((1.0 / denominator) * sign);
+			sign = (sign * -1);
+			denominator = denominator + 2;
+			i++;	
 		}
+		System.out.println("Approximation of pi: " + (totalPi * 4));
 	}
 }
